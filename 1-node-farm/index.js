bf8b037252128c0a5f705321d225e39cbb4b9608
Ritik Panday <panday.ritik@tftus.com>
2022-03-01 16:43:03 +0530

    first app

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
 const fs=require('fs');
 const http=require('http');
+const url=require('url');
+
 
 ////////////////////
 //FILES
@@ -29,11 +31,58 @@ const http=require('http');
 
 ////////////////////
 //SERVER
+const replaceTemplate=(temp,product)=>{
+    let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output=output.replace(/{%IMAGE%}/g,product.image);
+    output=output.replace(/{%PRICE%}/g,product.price);
+    output=output.replace(/{%FROM%}/g,product.from);
+    output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output=output.replace(/{%QUANTITY%}/g,product.quantity);
+    output=output.replace(/{%DESCRIPTION%}/g,product.description);
+    output=output.replace(/{%ID%}/g,product.id);
+    if(!product.organic)  output=output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+    return output;
+}
+const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+ const dataObj=JSON.parse(data);
+ 
 const server=http.createServer((req,res)=>{
-    console.log(req);
-    res.end('Hello from server');
+    const pathName=req.url;
+
+    if(pathName==='/'|| pathName==='/overview'){
+        res.writeHead(200,{'content-type':'text/html'});
+
+        const cardHtml=dataObj.map(el=>replaceTemplate(tempCard,el)).join('');
+        const output=tempOverview.replace('{%PRODUCT_CARDS%}',cardHtml);
+        
+        res.end(output);
+    }
+    else if(pathName==='/product'){
+        res.end('This is PRODUCT');
+    }
+    else if(pathName==='/api'){
+        res.writeHead(200,{'content-type':'application/json'});
+            res.end(data);
+        // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
+        //     const productData=JSON.parse(data);
+        //     res.writeHead(200,{'content-type':'application/json'});
+        //     res.end(data);
+        // })
+        
+    }
+    else{
+        res.writeHead(404,{
+            'content-type':'text/html'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+    
 }); 
 
-server.listen(8000,'127.0.0.1',()=>{
+server.listen(8000,'127.0.0.3',()=>{
     console.log('listening to request on port 8000');
 });
